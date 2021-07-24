8f37d2bb6311aab56e28dda81e1c2dd8947abadd
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2021-07-23 23:06:07 -0400

    Working sim?

--- include/psim/truth/satellite.hpp
+++ include/psim/truth/satellite.hpp
@@ -11,9 +11,84 @@ class TwoPointSatellite : public TwoPointSatelliteInterface<TwoPointSatellite> {
   typedef TwoPointSatelliteInterface<TwoPointSatellite> Super;
 
  protected:
-  static constexpr size_t N_ODE = 13; // Number of components in ODE
+  /**
+   * Gets the locations of the two satellite components in body frame.
+   * 
+   * @param[in] l Length of satellite.
+   * @param[in] mA Mass of satellite component A.
+   * @param[in] mB Mass of satellite component B.
+   * @param[out] xA_body Location of component A, in body frame.
+   * @param[out] xB_body Location of component B, in body frame.
+   */
+  static void get_mass_locations_body(Real l, Real mA, Real mB,
+    Vector3& xA_body, Vector3& xB_body);
+
+  /**
+   * Gets the locations of the two satellite components in ECI frame.
+   * 
+   * @param[in] x_eci Location of satellite COM in ECI.
+   * @param[in] q_eci_body Quaternion transforming vectors from body frame to ECI.
+   * @param[in] l Length of satellite.
+   * @param[in] mA Mass of satellite component A.
+   * @param[in] mB Mass of satellite component B.
+   * @param[out] xA_eci Location of component A, in ECI.
+   * @param[out] xB_eci Location of component B, in ECI.
+   */
+  static void get_mass_locations_eci(Vector3 const& x_eci,
+    Vector4 const& q_eci_body, Real l, Real mA, Real mB, Vector3& xA_eci,
+    Vector3& xB_eci);
+
+  /**
+   * Gets the locations of the two satellite components in ECI frame.
+   * This function uses the state-field values of r, d, m_A, m_B and q in order
+   * to compute this.
+   * 
+   * @param[out] xA Location of component A, in ECI.
+   * @param[out] xB Location of component B, in ECI.
+   */
+  void get_mass_locations_eci(Vector3& xA_eci, Vector3& xB_eci) const;
+
+  /**
+   * Utility function for conversion between vector angular rate
+   * and a quaternion representing the rotation rate.
+   * 
+   * @param w_body Angular rate in body frame
+   * @param q_body_eci Quaternion rotating from ECI to body
+   * @return Derivative of quaternion in ECI frame.
+   */
+  static Vector4 body_w_to_qdot(Vector3 const& w_body,
+    Vector4 const& q_body_eci);
+
+  /**
+   * Utility function for conversion between vector angular rate
+   * and a quaternion representing the rotation rate.
+   * 
+   * @param qdot_body_eci Derivative of quaternion that rotates from ECI to
+   *                      body frame
+   * @param q_body_eci Quaternion rotating from ECI to body frame
+   * @return Angular rate vector in body frame
+   */
+  static Vector3 qdot_to_body_w(Vector4 const& qdot_body_eci,
+    Vector4 const& q_body_eci);
+
+  struct IntegratorData
+  {
+    Real const &mA;
+    Real const &mB;
+    Real const &d;
+    Vector4 const &q_eci_ecef;
+  };
+
+  // Number of components in dynamics solver
+  static constexpr size_t N_ODE = 13;
+
+  // ODE integrator
   gnc::Ode4<Real, N_ODE> ode;
-  void get_mass_locations(Vector3& xA, Vector3& xB) const;
+
+  /**
+   * Applies full dual-point mass dynamics to the spacecraft.
+   */
+  static Vector<N_ODE> dynamics(Real t, Vector<N_ODE> const &x, void *ptr);
 
  public:
   using Super::TwoPointSatelliteInterface;
@@ -23,6 +98,12 @@ class TwoPointSatellite : public TwoPointSatelliteInterface<TwoPointSatellite> {
 
   virtual void step() override;
 
+  /**
+   * Lazy state fields.
+   */
+
+  Vector3 truth_satellite_orbit_A_r() const;
+  Vector3 truth_satellite_orbit_B_r() const;
   Real truth_satellite_orbit_T() const;
   Real truth_satellite_orbit_U() const;
   Real truth_satellite_orbit_E() const;
--- src/psim/truth/satellite.cpp
+++ src/psim/truth/satellite.cpp
@@ -12,111 +12,201 @@
 
 namespace psim {
 
-void TwoPointSatellite::get_mass_locations(Vector3& xA, Vector3& xB) const {
-  auto const &x = truth_satellite_orbit_r.get();
+static constexpr Real half = 0.5;
+
+Vector3 TwoPointSatellite::qdot_to_body_w(Vector4 const& qdot_body_eci, Vector4 const& q_body_eci)
+{
+  Vector4 half_w, qc;
+  gnc::utl::quat_conj(q_body_eci, qc);
+  gnc::utl::quat_cross_mult(qc, qdot_body_eci, half_w);
+  return 2*lin::ref<Vector3>(half_w, 0, 0);
+}
+
+Vector4 TwoPointSatellite::body_w_to_qdot(Vector3 const& w_body, Vector4 const& q_body_eci)
+{
+  Vector4 half_w, ret;
+  lin::ref<Vector3>(half_w, 0, 0) = half*w_body;
+  gnc::utl::quat_cross_mult(q_body_eci, half_w, ret);
+  return ret;
+}
+
+Vector<TwoPointSatellite::N_ODE>
+TwoPointSatellite::dynamics(Real t, Vector<TwoPointSatellite::N_ODE> const &x, void *ptr)
+{
+  /*
+   * Step 0. Unpack and prepare values that are useful throughout the computation.
+   */
+  auto const *data = static_cast<TwoPointSatellite::IntegratorData *>(ptr);
+
+  auto const &m_A = data->mA;
+  auto const &m_B = data->mB;
+  auto const &d = data->d;
+  auto const &q_eci_ecef = data->q_eci_ecef;
+
+  auto const r_eci = lin::ref<Vector3>(x, 0, 0);
+  auto const v_eci = lin::ref<Vector3>(x, 3, 0);
+  Vector4 q_body_eci = lin::ref<Vector4>(x, 6, 0);
+  auto const w_body = lin::ref<Vector3>(x, 10, 0);
+
+  Vector<N_ODE> dx;
+
+  Real m = m_A + m_B;
+  Vector4 q_ecef_eci, q_eci_body;
+  gnc::utl::quat_conj(q_eci_ecef, q_ecef_eci);
+  gnc::utl::quat_conj(q_body_eci, q_eci_body);
+
+  /*
+   * Step 1. Compute vdot.
+   */
+
+  /*
+   * Step 1.1. Get locations of satellite components in ECEF.
+   */
+  Vector3 xA, xB;
+  get_mass_locations_eci(r_eci, q_eci_body, d, m_A, m_B, xA, xB);
+  gnc::utl::rotate_frame(q_ecef_eci, xA);
+  gnc::utl::rotate_frame(q_ecef_eci, xB);
+
+  /*
+   * Step 1.2. Call GEOGRAV.
+   */
+  Vector3 F_a = m_A * orbit::gravity(xA);
+  Vector3 F_b = m_B * orbit::gravity(xB);
+
+  /*
+   * Step 1.3. Transform gravity vector into ECI.
+   */
+  gnc::utl::rotate_frame(q_eci_ecef, F_a);
+  gnc::utl::rotate_frame(q_eci_ecef, F_b);
+
+  /*
+   * Step 1.4. Sum forces and compute total force.
+   */
+  Vector3 a_eci = (F_a + F_b) / m;
+
+  /*
+   * Step 2. Compute wdot.
+   */
+  
+  /*
+   * Step 2.1. Transform gravity forces into body frame.
+   */
+  gnc::utl::rotate_frame(q_body_eci, F_a);
+  gnc::utl::rotate_frame(q_body_eci, F_b);
+
+  /*
+   * Step 2.2. Compute rotational inertia.
+   */
+  get_mass_locations_body(d, m_A, m_B, xA, xB);
+  Real rA = lin::norm(xA), rB = lin::norm(xB);
+  Real J = rA*rA*m_A + rB*rB*m_B;
+
+  /*
+   * Step 2.3. Compute angular acceleration.
+   */
+  Vector3 alpha_body = (lin::cross(xA, F_a) + lin::cross(xB, F_b)) / J;
+
+  /*
+   * Step 3. Compute change in quaternion.
+   */
+  Vector4 qdot_body_eci = body_w_to_qdot(w_body, q_body_eci);
+
+  /**
+   * Step 4. Populate the derivative vector
+   */
+  lin::ref<Vector3>(dx, 0, 0) = v_eci;
+  lin::ref<Vector3>(dx, 3, 0) = a_eci;
+  lin::ref<Vector4>(dx, 6, 0) = qdot_body_eci;
+  lin::ref<Vector3>(dx, 10, 0) = alpha_body;
+
+  return dx;
+}
+
+void TwoPointSatellite::get_mass_locations_body(Real l, Real mA, Real mB,
+  Vector3& xA_body, Vector3& xB_body)
+{
+  Real m = mA + mB;
+  Vector3 d; d(0) = l;
+  xA_body = - d * mB / m;
+  xB_body = d * mA / m;
+}
+
+void TwoPointSatellite::get_mass_locations_eci(Vector3 const& x_eci,
+  Vector4 const& q_eci_body, Real l, Real mA, Real mB, Vector3& xA_eci,
+  Vector3& xB_eci)
+{
+  get_mass_locations_body(l, mA, mB, xA_eci, xB_eci);
+  
+  gnc::utl::rotate_frame(q_eci_body, xA_eci);
+  gnc::utl::rotate_frame(q_eci_body, xB_eci);
+  xA_eci = xA_eci + x_eci;
+  xB_eci = xB_eci + x_eci;
+}
+
+void TwoPointSatellite::get_mass_locations_eci(Vector3& xA_eci,
+  Vector3& xB_eci) const
+{
+  auto const &x_eci = truth_satellite_orbit_r.get();
   auto const &l = truth_satellite_length.get();
   auto const &q_body_eci = truth_satellite_attitude_q_body_eci.get();
   Real mA = truth_satellite_A_m.get();
   Real mB = truth_satellite_B_m.get();
-  Real m = mA + mB;
-  
-  Vector3 d; // Vector from mass A to mass B
-  d[0] = l;
-  gnc::utl::rotate_frame(q_body_eci, d);
 
-  xA = x - d * mB / m;
-  xB = x + d * mA / m;
+  Vector4 q_eci_body;
+  gnc::utl::quat_conj(q_body_eci, q_eci_body);
+  get_mass_locations_eci(x_eci, q_eci_body, l, mA, mB, xA_eci, xB_eci);
 }
 
-void TwoPointSatellite::step() {
+void TwoPointSatellite::step()
+{
   this->Super::step();
 
-  struct IntegratorData
-  {
-    Real const &m;
-    Vector3 const &earth_w;
-    Vector3 const &earth_w_dot;
-  };
-
   auto const &dt = truth_dt_s->get();
-  auto const &earth_w = truth_earth_w->get();
-  auto const &earth_w_dot = truth_earth_w_dot->get();
   auto const &q_eci_ecef = truth_earth_q_eci_ecef->get();
   auto const &mA = truth_satellite_A_m.get();
   auto const &mB = truth_satellite_B_m.get();
-  Real m = mA + mB;
+  auto const &d = truth_satellite_length.get();
 
   auto &r_eci = truth_satellite_orbit_r.get();
   auto &v_eci = truth_satellite_orbit_v.get();
   auto &q_body_eci = truth_satellite_attitude_q_body_eci.get();
   auto &w_body = truth_satellite_attitude_w.get();
 
-  // Rotate vectors into ECEF: required for geograv
-  Vector4 q_ecef_eci;
-  Vector3 r_ecef, v_ecef;
-  gnc::utl::quat_conj(q_eci_ecef, q_ecef_eci);
-  gnc::utl::rotate_frame(q_ecef_eci, r_eci, r_ecef);
-  gnc::utl::rotate_frame(q_ecef_eci, v_eci, v_ecef);
+  IntegratorData data{mA, mB, d, q_eci_ecef};
 
   // Prepare integrator inputs.
   Vector<N_ODE> x;
-  lin::ref<Vector3>(x, 0, 0) = r_ecef;
-  lin::ref<Vector3>(x, 3, 0) = v_ecef;
+  lin::ref<Vector3>(x, 0, 0) = r_eci;
+  lin::ref<Vector3>(x, 3, 0) = v_eci;
   lin::ref<Vector4>(x, 6, 0) = q_body_eci;
   lin::ref<Vector3>(x, 10, 0) = w_body;
-  IntegratorData data{m, earth_w, earth_w_dot};
 
   // Simulate dynamics.
-  x = ode(Real(0.0), dt, x, &data,
-      [](Real t, Vector<N_ODE> const &x, void *ptr) -> Vector<N_ODE> {
-        auto const *data = static_cast<IntegratorData *>(ptr);
-
-        auto const &m = data->m;
-        auto const earth_w = (data->earth_w + t * data->earth_w_dot).eval();
-        auto const &earth_w_dot = data->earth_w_dot;
-
-        auto const r_ecef = lin::ref<Vector3>(x, 0, 0);
-        auto const v_ecef = lin::ref<Vector3>(x, 3, 0);
-        auto const q_body_eci = lin::ref<Vector4>(x, 6, 0);
-        auto const w_body = lin::ref<Vector3>(x, 10, 0);
-
-        Vector<N_ODE> dx;
-
-        // Orbital dynamics
-        {
-          Vector3 const a_ecef = orbit::acceleration(
-              earth_w, earth_w_dot, r_ecef.eval(), v_ecef.eval(), 0, m);
-
-          lin::ref<Vector3>(dx, 0, 0) = v_ecef;
-          lin::ref<Vector3>(dx, 3, 0) = a_ecef;
-        }
-
-        // Attitude dynamics - quaternion
-        {
-          Vector4 dq_body_eci;
-          Vector4 const dq = {
-              0.5 * w_body(0), 0.5 * w_body(1), 0.5 * w_body(2), 0.0};
-          gnc::utl::quat_cross_mult(dq, q_body_eci.eval(), dq_body_eci);
-
-          lin::ref<Vector4>(dx, 6, 0) = dq_body_eci;
-        }
-
-        return dx;
-      });
+  x = ode(Real(0.0), dt, x, &data, TwoPointSatellite::dynamics);
 
   // Write back to our state fields
-  r_ecef = lin::ref<Vector3>(x, 0, 0);
-  v_ecef = lin::ref<Vector3>(x, 3, 0);
-  gnc::utl::rotate_frame(q_eci_ecef, r_ecef, r_eci);
-  gnc::utl::rotate_frame(q_eci_ecef, v_ecef, v_eci);
-
+  r_eci = lin::ref<Vector3>(x, 0, 0);
+  v_eci = lin::ref<Vector3>(x, 3, 0);
   q_body_eci = lin::ref<Vector4>(x, 6, 0);
   w_body = lin::ref<Vector3>(x, 10, 0);
 }
 
-Real TwoPointSatellite::truth_satellite_orbit_T() const {
-  static constexpr Real half = 0.5;
+Vector3 TwoPointSatellite::truth_satellite_orbit_A_r() const
+{
+  Vector3 xA, xB;
+  get_mass_locations_eci(xA, xB);
+  return xA;
+}
 
+Vector3 TwoPointSatellite::truth_satellite_orbit_B_r() const
+{
+  Vector3 xA, xB;
+  get_mass_locations_eci(xA, xB);
+  return xB;
+}
+
+Real TwoPointSatellite::truth_satellite_orbit_T() const
+{
   auto const &v = truth_satellite_orbit_v.get();
   auto const &w = truth_satellite_attitude_w.get();
   Real mA = truth_satellite_A_m.get();
@@ -132,7 +222,7 @@ Real TwoPointSatellite::truth_satellite_orbit_T() const {
   // 2. Computing moment of inertia J
   // 3. Computing J w^2
   Vector3 xA, xB;
-  get_mass_locations(xA, xB);
+  get_mass_locations_eci(xA, xB);
   Real rA = lin::norm(xA);
   Real rB = lin::norm(xB);
   Real J = rA*rA*mA + rB*rB*mB;
@@ -142,9 +232,10 @@ Real TwoPointSatellite::truth_satellite_orbit_T() const {
   return half * (tKE + rKE);
 }
 
-Real TwoPointSatellite::truth_satellite_orbit_U() const {
+Real TwoPointSatellite::truth_satellite_orbit_U() const
+{
   Vector3 xA, xB;
-  get_mass_locations(xA, xB);
+  get_mass_locations_eci(xA, xB);
   Real rA = lin::norm(xA);
   Real rB = lin::norm(xB);
   Real mA = truth_satellite_A_m.get();
@@ -153,7 +244,8 @@ Real TwoPointSatellite::truth_satellite_orbit_U() const {
   return gnc::constant::mu_earth * (mA / rA + mB / rB);
 }
 
-Real TwoPointSatellite::truth_satellite_orbit_E() const {
+Real TwoPointSatellite::truth_satellite_orbit_E() const
+{
   Real T = truth_satellite_orbit_T();
   Real U = truth_satellite_orbit_U();
 
